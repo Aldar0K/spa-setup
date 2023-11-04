fc355c31094371fdd5edff2a9763bd1e44404e09
Aldar <aldarokonov21@gmail.com>
2023-11-04 13:48:51 +0300

    feat: add unit tests for classNames shared helper

--- src/shared/lib/classNames.test.ts
+++ src/shared/lib/classNames.test.ts
@@ -2,6 +2,25 @@ import { classNames } from './classNames';
 
 describe('classNames', () => {
   test('test', () => {
-    expect(classNames('someClass')).toBe('someClass');
+    expect(classNames('someClass'))
+      .toBe('someClass');
+  });
+
+  test('with additional classes', () => {
+    const expected = 'someClass class1 class2';
+    expect(classNames('someClass', {}, ['class1', 'class2']))
+      .toBe(expected);
+  });
+
+  test('with mods', () => {
+    const expected = 'someClass hovered class1 class2';
+    expect(classNames('someClass', { hovered: true, scrollable: false }, ['class1', 'class2']))
+      .toBe(expected);
+  });
+
+  test('with mods undefined', () => {
+    const expected = 'someClass hovered class1 class2';
+    expect(classNames('someClass', { hovered: true, scrollable: undefined }, ['class1', 'class2']))
+      .toBe(expected);
   });
 });
