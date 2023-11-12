8d963e4562fb083b1062183c04bb0d89d6600a9b
Aldar <aldarokonov21@gmail.com>
2023-11-12 13:14:31 +0300

    feat: update i18next/no-literal-string rule

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,11 +1,14 @@
-/* eslint-disable i18next/no-literal-string */
-
 import { render, screen } from '@testing-library/react';
-import { Button } from 'shared/ui/Button';
+import { Button, ButtonThemes } from 'shared/ui/Button';
 
 describe('Button', () => {
-  test('test', () => {
+  test('to be rendered', () => {
     render(<Button>Test</Button>);
     expect(screen.getByText('Test')).toBeInTheDocument();
+  });
+
+  test('to be clear', () => {
+    render(<Button theme={ButtonThemes.CLEAR}>Test</Button>);
+    expect(screen.getByText('Test')).toHaveClass('clear');
   });
 });
