e31bb259b3d677640404ddafd479ebb2242a330c
Aldar <aldarokonov21@gmail.com>
2023-11-12 12:59:17 +0300

    feat: setup react-testing-library

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable i18next/no-literal-string */
+
+import { render, screen } from '@testing-library/react';
+import { Button } from 'shared/ui/Button';
+
+describe('Button', () => {
+  test('test', () => {
+    render(<Button>Test</Button>);
+    expect(screen.getByText('Test')).toBeInTheDocument();
+  });
+});
