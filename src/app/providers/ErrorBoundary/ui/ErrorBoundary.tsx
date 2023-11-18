3ca11e7b179b04563374c1bb5ef316b0bdd7144c
Aldar <aldarokonov21@gmail.com>
2023-11-18 12:49:34 +0300

    fix: resolve eslint problems

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -17,14 +17,16 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     this.state = { hasError: false };
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   static getDerivedStateFromError(error: Error) {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     // You can also log the error to an error reporting service
-    console.log(error, errorInfo);
+    // console.log(error, errorInfo);
   }
 
   render() {
