19044b2dc3d78d61b5cd466c4f577e79d8549f00
Aldar <aldarokonov21@gmail.com>
2023-11-26 13:54:31 +0300

    refactor: update pipeline

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,15 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 
-import { buildCssLoader } from './loaders/buildCssLoader';
-import { buildSvgLoader } from './loaders/buildSvgLoader';
 import { BuildOptions } from "./types";
 
 export const buildLoaders = ({
   isDev,
 }: BuildOptions): webpack.RuleSetRule[] => {
-  const svgLoader = buildSvgLoader();
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
 
   const fileLoader: webpack.RuleSetRule = {
     test: /\.(png|jpe?g|gif|woff|woff2)$/i,
@@ -44,7 +46,24 @@ export const buildLoaders = ({
     exclude: /node_modules/,
   };
 
-  const cssLoader = buildCssLoader(isDev);
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: /\.module\.\w+$/i,
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
 
   return [svgLoader, fileLoader, babelLoader, typescriptLoader, cssLoader];
 };
