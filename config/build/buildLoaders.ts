4189aee6d45d19537e1443078cab6b03c96c61dd
Aldar <aldarokonov21@gmail.com>
2023-11-26 13:59:40 +0300

    refactor: update pipeline

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 
+import { buildCssLoader } from './loaders';
 import { BuildOptions } from "./types";
 
 export const buildLoaders = ({
@@ -46,24 +46,7 @@ export const buildLoaders = ({
     exclude: /node_modules/,
   };
 
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: /\.module\.\w+$/i,
-            localIdentName: isDev
-              ? "[path][name]__[local]--[hash:base64:5]"
-              : "[hash:base64:8]",
-          },
-        },
-      },
-      "sass-loader",
-    ],
-  };
+  const cssLoader = buildCssLoader(isDev);
 
   return [svgLoader, fileLoader, babelLoader, typescriptLoader, cssLoader];
 };
