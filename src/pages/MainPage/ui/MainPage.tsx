d1e4f72ee79a5d834a52768c755d5cbf03714329
Aldar <aldarokonov21@gmail.com>
2023-12-10 13:19:26 +0300

    feat: update Modal

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,34 @@
-import { FC } from 'react';
+/* eslint-disable i18next/no-literal-string */
+
+import { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import {
+  Button, ButtonSizes, ButtonThemes, Modal,
+} from 'shared/ui';
 import styles from './MainPage.module.scss';
 
 const MainPage: FC = () => {
   const { t } = useTranslation('main');
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <div className={styles.container} data-testid="MainPage">
-      {t('Main')}
+      <Button
+        theme={ButtonThemes.OUTLINE}
+        size={ButtonSizes.S}
+        onClick={() => setModalOpen(true)}
+      >
+        Open Modal
+      </Button>
+
+      <Modal
+        isOpen={modalOpen}
+        title="Some title"
+        onClose={() => setModalOpen(false)}
+      >
+        {t('Main')}
+      </Modal>
     </div>
   );
 };
