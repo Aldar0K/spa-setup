b05d436775f40b48139601aa457652a0032600dc
Aldar <aldarokonov21@gmail.com>
2023-12-17 14:38:19 +0300

    feat: setup user entity

--- src/app/providers/StoreProvider/model/reducers.ts
+++ src/app/providers/StoreProvider/model/reducers.ts
@@ -1,9 +1,11 @@
 import { Action, combineReducers } from '@reduxjs/toolkit';
 
 import { counterReducer } from 'entities/counter';
+import { userReducer } from 'entities/user';
 
 export const reducer = combineReducers({
   counter: counterReducer,
+  user: userReducer,
 });
 
 const rootReducer = (state: StateSchema, action: Action): StateSchema => {
---
+++ src/entities/user/index.ts
@@ -0,0 +1,2 @@
+export { userActions, userReducer } from './model/slice';
+export { User } from './model/types/user';
