5a0c1c9dfe5d4a170b92ecefec1e9a592587b65d
Aldar <aldarokonov21@gmail.com>
2024-02-03 13:58:15 +0300

    feat: add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,41 +1,32 @@
-import webpack from "webpack";
+import webpack from 'webpack';
 
 import { buildCssLoader } from './loaders';
-import { BuildOptions } from "./types";
+import { buildBabelLoader } from './loaders/buildBabelLoader';
+import { BuildOptions } from './types';
 
-export const buildLoaders = ({
-  isDev,
-}: BuildOptions): webpack.RuleSetRule[] => {
+export const buildLoaders = (options: BuildOptions): webpack.RuleSetRule[] => {
+  const { isDev } = options;
+
   const svgLoader = {
     test: /\.svg$/,
-    use: ["@svgr/webpack"],
+    use: ['@svgr/webpack']
   };
 
   const fileLoader: webpack.RuleSetRule = {
     test: /\.(png|jpe?g|gif|woff|woff2)$/i,
     use: [
       {
-        loader: "file-loader",
-      },
-    ],
+        loader: 'file-loader'
+      }
+    ]
   };
 
-  const babelLoader: webpack.RuleSetRule = {
-    test: /\.(js|jsx|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: "babel-loader",
-      options: {
-        presets: ["@babel/preset-env"],
-        plugins: [],
-      },
-    },
-  };
+  const babelLoader: webpack.RuleSetRule = buildBabelLoader(options);
 
   const typescriptLoader: webpack.RuleSetRule = {
     test: /\.tsx?$/,
-    use: "ts-loader",
-    exclude: /node_modules/,
+    use: 'ts-loader',
+    exclude: /node_modules/
   };
 
   const cssLoader = buildCssLoader(isDev);
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,47 +1,50 @@
-import CopyWebpackPlugin from "copy-webpack-plugin";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from "webpack";
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-import { BuildOptions } from "./types";
+import { BuildOptions } from './types';
 
 export const buildPlugins = ({
   paths,
-  isDev,
+  isDev
 }: BuildOptions): webpack.WebpackPluginInstance[] => {
   const plugins = [
-   new HtmlWebpackPlugin({
-      template: paths.html,
+    new HtmlWebpackPlugin({
+      template: paths.html
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css",
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
     }),
     new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
+      __IS_DEV__: JSON.stringify(isDev)
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
           from: paths.locales,
-          to: paths.buildLocales,
+          to: paths.buildLocales
         },
         {
           from: paths.meta,
-          to: paths.buildMeta,
+          to: paths.buildMeta
         }
-      ],
+      ]
     })
   ];
 
   if (isDev) {
     plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }));
+      new ReactRefreshWebpackPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
+      })
+    );
   }
 
   return plugins;
